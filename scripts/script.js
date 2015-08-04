984a97bccac9ac362bc5079f2b0855010a9f513b
Tyler Guillen <tyguillen@gmail.com>
2015-08-04 02:42:08 -0700

    resize, opacity change, random color working

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,48 @@
+var currentOption = 0; // Init global option;
+
 $(document).ready(function() {
-	var $square = $('<var class="gridsquare"></var>');
-	function populateGrid(n) {
-		$("#gridwrapper").html(''); // Empty grid
-		
-		for(var i = 0; i < n; i++) { // Rows
-			for(var j = 0; j < n; j++){ // Row Elements
-				$('#gridwrapper').append('<div class="gridsquare"></div>');
-			}
-			$('#gridwrapper').append('<div class="newrow"></div>');
+	populateGrid(16, 0); // Initialize grid.
+});
+
+var resize = function() { // Resize the blocks.
+	var value = +$('#number').val(); // Get text from field.
+	if(value >= 1 && value <= 128) populateGrid(value, currentOption); // Check for invalid input.
+	else alert("Invalid input."); // Throw error.
+};
+
+var optChange = function(option) {
+	currentOption = option;
+	var value = +$('#number').val();
+	populateGrid(value, option);
+};
+
+function populateGrid(squareSize, option) { //Main function: 
+	$("#gridwrapper").html(''); // Empty grid
+	
+	for(var i = 0; i < squareSize; i++) { // Rows
+		for(var j = 0; j < squareSize; j++){ // Row Elements
+			$('#gridwrapper').append('<div class="gridsquare"></div>');
 		}
-		
-		var dim = 592 / n; // Resize squares to fit
-		$('.gridsquare').css('height', dim);
-		$('.gridsquare').css('width', dim);
+		$('#gridwrapper').append('<div class="newrow"></div>');
 	}
 	
-	populateGrid(16);
+	var dim = 592 / squareSize; // Resize squares to fit
+	$('.gridsquare').css('height', dim);
+	$('.gridsquare').css('width', dim);
 	
 	$('.gridsquare').on('mouseenter', function() {
-		$(this).css("background-color", '#fff');
+		switch(option){
+			case 0:
+				$(this).css("background-color", '#fff');
+				break;
+			case 1:
+				var opacity = $(this).css('opacity');
+				if(opacity > 0) $(this).css('opacity', opacity - .1);
+				break;
+			case 2:
+				var getColor = function() {return Math.floor(Math.random() * 256);};
+				$(this).css('background-color', 'rgb(' + getColor() + ', ' + getColor() + ', ' + getColor() + ')');
+				break;
+		}
 	});
-	
-	$('#resize').on('click', function() {
-		var value = +$('#number').val();
-		if(value >= 1 && value <= 128) populateGrid(value);
-		else alert("Invalid input.");
-	});
-});
+}
