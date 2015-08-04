f5dae05b933260e69534dd2638f993211461b5ac
Tyler Guillen <tyguillen@gmail.com>
2015-08-04 00:04:59 -0700

    nightly commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var $square = $('<var class="gridsquare"></var>');
-	var populateGrid = function(n) {
+	function populateGrid(n) {
 		$("#gridwrapper").html(''); // Empty grid
 		
 		for(var i = 0; i < n; i++) { // Rows
@@ -9,6 +9,21 @@ $(document).ready(function() {
 			}
 			$('#gridwrapper').append('<div class="newrow"></div>');
 		}
+		
+		var dim = 592 / n; // Resize squares to fit
+		$('.gridsquare').css('height', dim);
+		$('.gridsquare').css('width', dim);
 	}
+	
 	populateGrid(16);
+	
+	$('.gridsquare').on('mouseenter', function() {
+		$(this).css("background-color", '#fff');
+	});
+	
+	$('#resize').on('click', function() {
+		var value = +$('#number').val();
+		if(value >= 1 && value <= 128) populateGrid(value);
+		else alert("Invalid input.");
+	});
 });
